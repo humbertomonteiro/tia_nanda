26601a9bd05813263a1cbb1f4dcf35ffeb4a6834
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-04-22 16:48:10 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-const about = document.querySelectorAll('[about]')
 const main = document.querySelector('main')
+
+const about = document.querySelectorAll('[about]')
+
+const materials = document.querySelector('[materials]')
 
 const locationHere = document.querySelector('[location]')
 
@@ -11,40 +14,37 @@ const btnToggle = document.querySelector('.btn-toggle')
 
 about.forEach(e => {
     e.onclick = a => {
-        a.preventDefault()
-
-        fetch(e.getAttribute('about'))
-            .then(resp => resp.text())
-            .then(page => main.innerHTML = page)
-
-        closeOpenToggle() 
+        linkAjax(a, 'pages/about.html')
     }
 })
 
+materials.onclick = a => {
+    linkAjax(a, 'pages/materials.html')
+}
+
 locationHere.onclick = a => {
-    a.preventDefault()
-
-    fetch('pages/location.html')
-        .then(resp => resp.text())
-        .then(page => main.innerHTML = page)
-
-    closeOpenToggle() 
+    linkAjax(a, 'pages/location.html')
 }
 
 medias.onclick = a => {
-    a.preventDefault()
-
-    fetch('pages/media.html')
-        .then(resp => resp.text())
-        .then(page => main.innerHTML = page)
-
-    closeOpenToggle() 
+    linkAjax(a, 'pages/media.html')
 }
 
 
 btnToggle.onclick = () => {
     navbar.classList.toggle('navbar')
     navbar.classList.toggle('show-toggle')
+}
+
+
+function linkAjax(event, url) {
+    event.preventDefault()
+
+    fetch(url)
+        .then(resp => resp.text())
+        .then(page => main.innerHTML = page)
+
+    closeOpenToggle()
 }
 
 
