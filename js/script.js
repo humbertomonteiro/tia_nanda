75259c118872634a89db6a77f5d901af6f3306e0
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-11-17 14:53:48 -0300

    new layout

--- js/script.js
+++ js/script.js
@@ -1,63 +1,29 @@
-const main = document.querySelector('main')
-
-const about = document.querySelectorAll('[about]')
-
-const materials = document.querySelector('[materials]')
-
-const locationHere = document.querySelector('[location]')
-
-const medias = document.querySelector('[medias]')
-
-const navbar = document.querySelector('#navbar')
-const btnToggle = document.querySelector('.btn-toggle')
-
-
-about.forEach(e => {
-    e.onclick = a => {
-        linkAjax(a, 'pages/about.html')
-    }
-})
-
-materials.onclick = a => {
-    linkAjax(a, 'pages/materials.html')
-}
-
-locationHere.onclick = a => {
-    linkAjax(a, 'pages/location.html')
-}
-
-medias.onclick = a => {
-    linkAjax(a, 'pages/media.html')
-}
-
+const navbar = document.querySelector("#navbar");
+const btnToggle = document.querySelector(".btn-toggle");
 
 btnToggle.onclick = () => {
-    navbar.classList.toggle('navbar')
-    navbar.classList.toggle('show-toggle')
-}
-
+  navbar.classList.toggle("navbar");
+  navbar.classList.toggle("show-toggle");
+};
 
 function linkAjax(event, url) {
-    event.preventDefault()
+  event.preventDefault();
 
-    fetch(url)
-        .then(resp => resp.text())
-        .then(page => main.innerHTML = page)
+  fetch(url)
+    .then((resp) => resp.text())
+    .then((page) => (main.innerHTML = page));
 
-    closeOpenToggle()
+  closeOpenToggle();
 }
 
-
 function closeOpenToggle() {
+  const showOrHidden = navbar.classList.contains("navbar");
 
-    const showOrHidden = navbar.classList.contains('navbar')
-
-    if(showOrHidden) {
-        navbar.classList.add('navbar')
-        navbar.classList.remove('show-toggle')
-    } else {
-        navbar.classList.toggle('navbar')
-        navbar.classList.toggle('show-toggle')
-    }
-    
+  if (showOrHidden) {
+    navbar.classList.add("navbar");
+    navbar.classList.remove("show-toggle");
+  } else {
+    navbar.classList.toggle("navbar");
+    navbar.classList.toggle("show-toggle");
+  }
 }
